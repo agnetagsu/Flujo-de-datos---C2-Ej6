b539fb2310ae4243189e1390f260bbb44a4c55d3
Agneta <agnetagsu@yahoo.es>
2021-05-22 20:25:49 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int precio;
+            string pago;
+            double cuenta;
+
+            Console.WriteLine("Introduzca el precio");
+            precio = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Elija la forma de pago: efectivo o tarjeta");
+            pago = Console.ReadLine();
+
+            if(pago == "tarjeta")
+            {
+                Console.WriteLine("Introduzca el numero de cuenta");
+                cuenta = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+    }
+}
